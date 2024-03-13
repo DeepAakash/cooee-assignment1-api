a7384fcdb5fb8a58471aaec65832fd21572a8dba
Aakash Deep <aakashsinghkarnaal@gmail.com>
2024-03-13 17:12:57 +0530

    CORS

--- src/main.ts
+++ src/main.ts
@@ -16,14 +16,21 @@ async function bootstrap() {
   //     'http://localhost:4200']
   // })
 
+  // Define the array of allowed origins
+  const allowedOrigins: string[] = [
+    'http://localhost:4200',
+    'https://cooee-project1.vercel.app',
+    // Add more origins as needed
+  ];
+  
   // To allow port 4200 and vercel access the data
   const corsOptions: CorsOptions = {
-    origin: [
-      'http://localhost:4200', 
-      'https://cooee-project1.vercel.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: allowedOrigins,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Add the HTTP methods you want to allow
+    allowedHeaders: ['Content-Type', 'Authorization'], // Add the headers you want to allow
+    credentials: true, // Enable credentials such as cookies and authorization headers
   };
+
   app.enableCors(corsOptions);
 
   await app.listen(3000);
