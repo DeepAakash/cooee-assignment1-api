55cf15a10dd0f2fc96f432e98ae1825526b3c53a
Aakash Deep <aakashsinghkarnaal@gmail.com>
2024-03-15 02:02:17 +0530

    Bug fix for localhost CORS

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,47 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import { ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  // To set 'api' as global prefix for all api requests
   app.setGlobalPrefix('api');
-  app.enableCors({
-    origin: 'https://cooee-project1.vercel.app/',
-    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+
+  // To allow port 4200 access the data
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:4200']
+  // })
+
+  // Define the array of allowed origins
+  const allowedOrigins: string[] = [
+    'http://localhost:4200',
+    'https://cooee-project1.vercel.app',
+    // Add more origins as needed
+  ];
+
+  // To allow port 4200 and vercel access the data
+  const cors = require('cors');
+  const corsOptions ={
+    origin:allowedOrigins, 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
     preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-  });
+}
+
+  app.use(cors(corsOptions));
+
+  // await app.listen(3000);
+
   let port: number;
   if (process.env.NODE_ENV === 'production') {
+    // Use the port specified by the hosting environment in production
     port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
   } else {
-    port = 3000;
+    port = 3000; // Use port 3000 for development
   }
 
   await app.listen(port);
