53c65dda58927c6860f05dc7778ba37dc4d5fe52
Aakash Deep <aakashsinghkarnaal@gmail.com>
2024-03-14 19:41:31 +0530

    UpdateHeader to prevent CORS in vercel

--- src/cors.middleware.ts
+++ src/cors.middleware.ts
@@ -4,7 +4,7 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://cooee-project1.vercel.app');
     res.setHeader(
       'Access-Control-Allow-Methods',
       'GET, POST, PUT, DELETE, OPTIONS',
@@ -14,7 +14,6 @@ export class CorsMiddleware implements NestMiddleware {
       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
     );
     if (req.method === 'OPTIONS') {
-      // Respond to OPTIONS requests
       res.sendStatus(200);
     } else {
       next();
