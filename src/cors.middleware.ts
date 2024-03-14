ffd77e0f61a31900b6be2523ae907d4e9c36bb9e
Aakash Deep <aakashsinghkarnaal@gmail.com>
2024-03-15 01:38:51 +0530

    CORS

--- src/cors.middleware.ts
+++ src/cors.middleware.ts
@@ -1,22 +1,25 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
+import { Response, NextFunction } from 'express';
 
-@Injectable()
-export class CorsMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PUT, DELETE, OPTIONS',
-    );
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-    );
-    if (req.method === 'OPTIONS') {
-      res.sendStatus(200);
-    } else {
-      next();
-    }
+export const allowCors = (fn: (req: any, res: any) => Promise<void>) => async (req: any, res: Response, next: NextFunction) => {
+  res.setHeader('Access-Control-Allow-Credentials', 'true'); // Convert true to string 'true'
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
   }
-}
+  await fn(req, res);
+};
+
+const handler = async (req: any, res: any) => {
+  const d = new Date();
+  res.end(d.toString());
+};
+
+export default allowCors(handler);
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { CorsMiddleware } from './cors.middleware'; // Import CorsMiddleware
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
-
-  // Use CorsMiddleware globally
-  app.use(new CorsMiddleware().use);
-
+  app.enableCors({
+    origin: 'https://cooee-project1.vercel.app/',
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
   let port: number;
   if (process.env.NODE_ENV === 'production') {
     port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
