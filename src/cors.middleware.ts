1f592eb976c91efc47cab9ed7649fb99a759611a
Aakash Deep <aakashsinghkarnaal@gmail.com>
2024-03-14 18:08:28 +0530

    Add cors

---
+++ src/cors.middleware.ts
@@ -0,0 +1,23 @@
+// Middleware to handle CORS at time ho deployment
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class CorsMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, OPTIONS',
+    );
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    );
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  }
+}
